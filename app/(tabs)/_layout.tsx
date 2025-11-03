1a21b2d581f6bf57729b73ae89eef045d169541c
Lyna <lynaben2001@gmail.com>
2025-11-02 23:17:52 -0500

    -Nettoyage des fichiers inutiles créés lors de la création du projet avec 'npm run reset-project' -Ajout svg mascotte -Création du routage pour la barre de navigation en bas de page (navigation entre les pages les plus superficielles de l'application)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,46 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
+import {Tabs} from "expo-router";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const _Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+        <Tabs.Screen
+            name= "parametres"
+            options= {{
+                title: "Paramètres",
+                headerShown: false,
+            }}
+        />
+        <Tabs.Screen
+            name= "donneesEtudes"
+            options= {{
+                title: "Données",
+                headerShown: false,
+            }}
+        />
+        <Tabs.Screen
+            name= "index"
+            options= {{
+                title: "Accueil",
+                headerShown: false,
+            }}
+        />
+        <Tabs.Screen
+            name= "pomodoro"
+            options= {{
+                title: "Pomodoro",
+                headerShown: false,
+            }}
+        />
+        <Tabs.Screen
+            name= "profil"
+            options= {{
+                title: "Profil",
+                headerShown: false,
+            }}
+        />
     </Tabs>
   );
-}
+};
+
+export default _Layout;
---
+++ app/(tabs)/pomodoro.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Pomodoro = () => {
+  return (
+    <View>
+      <Text>Pomodoro</Text>
+    </View>
+  );
+};
+
+export default Pomodoro;
